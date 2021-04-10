cd460e414e5696c9f19c7b95c279c1ee513bfde2
lakshmaji-till <66242708+lakshmaji-till@users.noreply.github.com>
2021-04-10 22:06:39 +0530

    Create array.tsx

---
+++ spread/array.tsx
@@ -0,0 +1,6 @@
+/**
+ * With spread operator we can create new array by combining `minions` array and `boss` string! 
+ */
+const minions = ['Kevin', 'Bob', 'Stuart'];
+const boss = 'Gru';
+const superVillainGroup = [...minions, boss]; // [ 'Kevin', 'Bob', 'Stuart', 'Gru' ]
